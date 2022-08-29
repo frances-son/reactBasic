0b63dd0afb5957cf9495d9184e75ff7217d7dbfd
frances son <s.wisdom.one@gmail.com>
2022-08-30 00:15:29 +0900

    update

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,27 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from './Card';
 
 function ExpenseItem(props) {
+
+  let title = props.title;
+
+
+  // 앞쪽에 정의
+  const clickHandler = () => {
+    title = 'Updated!';
+    console.log(title);
+  };
+
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date = {props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
   );
 }
 
